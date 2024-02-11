f8a9e48381c0622cae186b3576e5b4de7d60db48
Vardens-42 <131184757+Vardens-42@users.noreply.github.com>
2024-02-10 19:40:24 -0500

    Links ASC to Enhanced Input

--- Source/DarkFort/Public/Characters/Abilities/DarkFortAbilitySystemComponent.h
+++ Source/DarkFort/Public/Characters/Abilities/DarkFortAbilitySystemComponent.h
@@ -4,15 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayAbilitySpec.h" // for FGameplayAbilitySpecHandle
+#include "EnhancedInputComponent.h" // for FInputBindingHandle
 #include "DarkFortAbilitySystemComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FReceivedDamageDelegate, UDarkFortAbilitySystemComponent*, SourceASC, float, UnmitigatedDamage, float, MitigatedDamage);
 
+class UInputAction;
 
-/**
- * 
- */
-UCLASS()
+USTRUCT()
+struct FAbilityInputBinding
+{
+	GENERATED_BODY()
+
+	int32 InputID = 0;
+	uint32 OnPressedHandle = 0;
+	uint32 OnReleasedHandle = 0;
+	TArray<FGameplayAbilitySpecHandle> BoundAbilitiesStack;
+};
+
+UCLASS(Blueprintable, BlueprintType, Category = "AbilityInput", meta = (BlueprintSpawnableComponent))
 class DARKFORT_API UDarkFortAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
@@ -25,4 +36,36 @@ public:
 	FReceivedDamageDelegate ReceivedDamage;
 
 	virtual void ReceiveDamage(UDarkFortAbilitySystemComponent* SourceASC, float UnmitigatedDamage, float MitigatedDamage);
+	
+	///////////////////////////////////
+	//Enhanced Input System integration
+	///////////////////////////////////
+
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void SetInputBinding(UInputAction* InputAction, FGameplayAbilitySpecHandle AbilityHandle);
+
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void ClearInputBinding(FGameplayAbilitySpecHandle AbilityHandle);
+
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void ClearAbilityBindings(UInputAction* InputAction);
+	
+private:
+	void OnAbilityInputPressed(UInputAction* InputAction);
+
+	void OnAbilityInputReleased(UInputAction* InputAction);
+
+	void RemoveEntry(UInputAction* InputAction);
+
+	void TryBindAbilityInput(UInputAction* InputAction, FAbilityInputBinding& AbilityInputBinding);
+
+	FGameplayAbilitySpec* FindAbilitySpec(FGameplayAbilitySpecHandle Handle);
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(transient)
+	TMap<UInputAction*, FAbilityInputBinding> MappedAbilities;
+
+	UPROPERTY(transient)
+	UEnhancedInputComponent* InputComponent;
 };
