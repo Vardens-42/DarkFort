6d0793f8e9c80be51745613f19479bbb7b04d43b
Vardens-42 <131184757+Vardens-42@users.noreply.github.com>
2024-02-10 19:21:50 -0500

    Extends the AI controller

---
+++ Source/DarkFort/Private/AI/DarkFortAIController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/DarkFortAIController.h"
+
+ADarkFortAIController::ADarkFortAIController()
+{
+	bWantsPlayerState = true;
+}
